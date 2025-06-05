87462d719a12f6cb078e692eadb166022f00376e
Ryhor Peraverzeu <boot762@gmail.com>
2025-06-05 09:01:45 +0200

    feat: add InputNumber component

---
+++ components/ui/InputNumber.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+const model = defineModel();
+</script>
+
+<template>
+  <div>
+    <input
+      v-model="model"
+      type="number"
+      placeholder="Вес (кг)"
+      class="text-2xl py-4 px-1 w-full rounded-md border-2 border-gray-400 mb-4"
+    />
+  </div>
+</template>
+
+<style lang="scss" scoped></style>
--- pages/new/index.vue
+++ pages/new/index.vue
@@ -1,9 +1,8 @@
 <script setup lang="ts">
-const val = ref<string>("");
-
-watch(val, () => {
-  console.log(val.value);
-});
+const weight = ref<string>("");
+const waist = ref<string>("");
+const hips = ref<string>("");
+const chest = ref<string>("");
 </script>
 
 <template>
@@ -11,6 +10,31 @@ watch(val, () => {
     <h2 class="text-5xl font-bold mb-7">Новый замер</h2>
     <div>
       <form>
+        <h3 class="text-3xl mb-4">Добавление данных</h3>
+        <input
+          v-model="weight"
+          type="number"
+          placeholder="Вес (кг)"
+          class="text-2xl py-4 px-1 w-full rounded-md border-2 border-gray-400 mb-4"
+        />
+        <input
+          v-model="waist"
+          type="number"
+          placeholder="Талия (см)"
+          class="py-4 px-1 w-full rounded-md border-2 border-gray-400 mb-4"
+        />
+        <input
+          v-model="chest"
+          type="number"
+          placeholder="Грудь (см)"
+          class="py-4 px-1 w-full rounded-md border-2 border-gray-400 mb-4"
+        />
+        <input
+          v-model="hips"
+          type="number"
+          placeholder="Бедра (см)"
+          class="py-4 px-1 w-full rounded-md border-2 border-gray-400 mb-10"
+        />
         <button
           type="submit"
           class="bg-green-800 active:bg-green-700 text-white text-2xl text-center rounded-md py-4 w-full"
